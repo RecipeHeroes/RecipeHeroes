cd0aeb051d443fe3cfa14cb53760b763b52a4eed
Oerdek <enes.oerdek@sap.com>
2022-05-26 12:54:23 +0200

    feat: first get request

--- app/src/main/java/com/example/recipes/RecipeViewer.java
+++ app/src/main/java/com/example/recipes/RecipeViewer.java
@@ -1,8 +1,15 @@
 package com.example.recipes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler;
+
+import okhttp3.Headers;
 
 public class RecipeViewer extends AppCompatActivity {
 
@@ -10,5 +17,8 @@ public class RecipeViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_viewer);
+
+
+
     }
 }
