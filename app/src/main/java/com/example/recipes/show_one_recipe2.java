0a330acf435410b94b072dbfaab808d932a058b8
tarekabouwarda <tarek.abouwarda@gmail.com>
2022-05-26 17:14:01 +0200

    Show One Recipe Activity

---
+++ app/src/main/java/com/example/recipes/show_one_recipe2.java
@@ -0,0 +1,14 @@
+package com.example.recipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class show_one_recipe2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_one_recipe2);
+    }
+}
