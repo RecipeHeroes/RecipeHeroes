03eab60a8c49f5671e95e451823c42b5bc8fd661
Aeneis97 <julia-archelon@web.de>
2022-06-07 17:18:05 +0200

    Concluded method to set oneRecipeViewer from backend data

--- app/src/main/java/com/example/recipes/show_one_recipe2.java
+++ app/src/main/java/com/example/recipes/show_one_recipe2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
@@ -26,20 +27,37 @@ public class show_one_recipe2 extends AppCompatActivity {
 
         AsyncHttpClient client = new AsyncHttpClient();
         String url = "http://10.0.2.2:4004/recipe-heroes/Recipe("+id+")";
+
         client.get(url, new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
+
+                // Set attributes from json Object
                 try {
                     String title = json.jsonObject.getString("Title");
+                    String description = json.jsonObject.getString("Description");
+                    String difficulty = json.jsonObject.getString("Difficulty");
+                    String duration_minutes = json.jsonObject.getString("DurationMinutes");
+                    int portions = (int)json.jsonObject.getInt("Portions");
+
                     Log.d("DEBUG", json.toString());
+                // Set View with attributes from json Object
+                    TextView title_view = findViewById(R.id.textView);
+                    title_view.setText(title);
+                    TextView difficulty_view = findViewById(R.id.recipe_difficulty);
+                    difficulty_view.setText(difficulty);
+                    TextView duration_minutes_view = findViewById(R.id.duration_recipeviewer);
+                    duration_minutes_view.setText(duration_minutes);
+                    TextView description_view = findViewById(R.id.description_text);
+                    description_view.setText(description);
+                    Spinner portions_view = findViewById(R.id.portions_recipe);
+                    portions_view.setSelection(portions);
 
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(title);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-               catch (JSONException jsonException){
 
-               }
             }
 
             @Override
@@ -48,5 +66,4 @@ public class show_one_recipe2 extends AppCompatActivity {
             }
         });
     }
-
 }
