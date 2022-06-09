94d024e4b2d5bbd4bc92952caa970465c56f36ac
Aeneis97 <julia-archelon@web.de>
2022-06-09 09:57:02 +0200

    Reshaped Layout and added new picture to front. Also changed buttons to top bar.

--- app/src/main/java/com/example/recipes/Overview.java
+++ app/src/main/java/com/example/recipes/Overview.java
@@ -1,15 +1,19 @@
 package com.example.recipes;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.recipes.ui.login.LoginActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +28,8 @@ public class Overview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         String logTag = Overview.class.getSimpleName();
 
         Log.v(logTag, "Verbose     - Meldung.");
@@ -32,19 +38,18 @@ public class Overview extends AppCompatActivity {
         Log.w(logTag, "Warning     - Meldung.");
         Log.e(logTag, "Error       - Meldung.");
 
-
-//        Button btn = (Button)findViewById(R.id.button_activity_main);
         Button btn_login = (Button)findViewById(R.id.login_button);
-        Button btn_settings = (Button)findViewById(R.id.settings_button);
+//        Button btn_settings = (Button)findViewById(R.id.settings_button);
+        TabLayout btn_settings = (TabLayout) findViewById(R.id.tabs);
         Button btn_recipelist = (Button)findViewById(R.id.recipelist_button);
         Button btn_test = (Button)findViewById(R.id.Testbutton);
 
-        btn_recipelist.setOnClickListener(new View.OnClickListener() {
+ /*       btn_recipelist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Overview.this, RecipeList.class));
             }
-        });
+        });*/
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,12 +58,37 @@ public class Overview extends AppCompatActivity {
             }
         });
 
-        btn_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Overview.this, SettingsActivity.class));
-            }
-        });
+          btn_settings.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+              @Override
+                 public void onTabSelected(TabLayout.Tab tab) {
+                        switch(tab.getPosition()) {
+                            case 1:
+                                startActivity(new Intent(Overview.this, RecipeList.class));
+                                break;
+                             case 2:
+                                 startActivity(new Intent(Overview.this, SettingsActivity.class));
+                                   }
+                                }
+
+                                @Override
+                               public void onTabUnselected(TabLayout.Tab tab) {
+
+                             }
+
+                              @Override
+                         public void onTabReselected(TabLayout.Tab tab) {
+
+                   }
+                 }
+
+
+          );
+//        btn_settings.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(Overview.this, SettingsActivity.class));
+//            }
+//        });
 
         Button btn_register = (Button)findViewById(R.id.register_button);
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +99,7 @@ public class Overview extends AppCompatActivity {
         });
 
         //Create Recipe
-        Button btn_createRecipe = (Button)findViewById(R.id.createRecipe_button);
+        FloatingActionButton btn_createRecipe = (FloatingActionButton) findViewById(R.id.floating_add_button);
         btn_createRecipe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
